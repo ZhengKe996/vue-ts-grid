760c5c4b720748c3badab964f6f106b0b2398d9f
ZhengKe <3119531802@qq.com>
2022-05-16 15:03:08 +0800

    fix: update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const SONG_ACCOMPANY: string =
-  "https://coco-img.oss-cn-hangzhou.aliyuncs.com/HZPT-TYG/school-song.mp3";
+  "https://zhengke-img.oss-cn-hangzhou.aliyuncs.com/TYG/school-song.mp3";
 export const SONG: string =
-  "https://coco-img.oss-cn-hangzhou.aliyuncs.com/HZPT-TYG/school-song.mp3";
+  "https://zhengke-img.oss-cn-hangzhou.aliyuncs.com/TYG/school-song.mp3";
