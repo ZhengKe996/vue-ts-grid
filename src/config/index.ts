f9a0d03268ec453262a83b8c0046cb2059cf61fc
ZhengKe <3119531802@qq.com>
2022-05-18 08:37:09 +0800

    test: 测试内联音频

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,9 @@
-export const SONG_ACCOMPANY: string =
-  "https://zhengke-img.oss-cn-hangzhou.aliyuncs.com/TYG/school-song.mp3";
-export const SONG: string =
-  "https://zhengke-img.oss-cn-hangzhou.aliyuncs.com/TYG/school-song.mp3";
+import Song from "@/assets/music/school-song.mp3";
+import SongAccompany from "@/assets/music/school-song-accompany.mp3";
+
+// export const SONG_ACCOMPANY: string =
+//   "https://zhengke-img.oss-cn-hangzhou.aliyuncs.com/TYG/school-song.mp3";
+// export const SONG: string =
+//   "https://zhengke-img.oss-cn-hangzhou.aliyuncs.com/TYG/school-song.mp3";
+export const SONG_ACCOMPANY = SongAccompany;
+export const SONG = Song;
